573bb89a91e95ad729bef9d1de450e9942d04f93
destinyd <destinyd_war@163.com>
2013-09-12 00:44:27 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WineCrawl::Application.config.secret_token = '980911e43dcb28028d204ffddb05fca2bc241370297f003986433a22a2fb8e2ae59d13de9bf96e49bf3fc08214ed84cb85adf3cc4a77947e1c6d795ed91c8e96'
