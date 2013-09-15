c1c1ef4410c983f94b3ee35634ae73bf1f4fcff6
destinyd <destinyd_war@163.com>
2013-09-15 14:05:16 +0800

    price with url

--- lib/jd_crawler.rb
+++ lib/jd_crawler.rb
@@ -1,13 +1,15 @@
 class JdCrawler < Crawler
   def initialize(args={})
     @api_url_format = 'http://p.3.cn/prices/get?skuid=J_%d'
+    @url_format = 'http://item.jd.com/%d.html'
   end
 
   def get(wine_monitor)
-    url = @api_url_format % wine_monitor.sn
-    t = Typhoeus.get(url, followlocation: true)
+    price_url = @api_url_format % wine_monitor.sn
+    url = @url_format % wine_monitor.sn
+    t = Typhoeus.get(price_url, followlocation: true)
     j = JSON.parse(t.body)[0]
-    ph = wine_monitor.wine.wine_prices.create current_price: j['p'], tag_price: j['m']
+    ph = wine_monitor.wine.wine_prices.create current_price: j['p'], tag_price: j['m'], url: url
   end
 
 end
