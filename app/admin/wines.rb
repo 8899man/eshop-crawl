2dfd6bb3e017e432dbcbe1829954e12c94c5accb
destinyd <destinyd_war@163.com>
2013-09-17 12:58:44 +0800

    admin wine show

--- app/admin/wines.rb
+++ app/admin/wines.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Wine do
-   form do |f|
+  form do |f|
     f.inputs do
       f.input :name
       f.input :introduction
@@ -10,7 +10,28 @@ ActiveAdmin.register Wine do
       f.input :categories
       f.input :image
     end
+    #f.inputs do
+    #f.has_many :wine_monitors do |wine_monitor|
+    #wine_monitor.input :lib, collection: YAML.load(ENV['CRAWLS']), include_blank: nil
+    #wine_monitor.input :sn
+    #wine_monitor.input :website, collection: Website.all, include_blank: nil
+    #end
+    #end
     f.actions
   end
- 
+
+
+  show title: :name do |wine|
+    attributes_table do
+      #row :name
+      row :current_price
+      row :min_price
+      row :fit_price
+      row :image do
+        image_tag(wine.image.url)
+      end
+      row :introduction
+      row :description
+    end
+  end
 end
