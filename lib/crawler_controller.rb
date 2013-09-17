e7a212cf6e04b289b0032903c38e2ef36f4eb377
destinyd <destinyd_war@163.com>
2013-09-17 12:17:00 +0800

    crawler controller

--- lib/crawler_controller.rb
+++ lib/crawler_controller.rb
@@ -1,12 +1,18 @@
 class CrawlerController
   def initialize(args={})
     @jd = JdCrawler.new
+    @dangdang = DangdangCrawler.new
   end
 
   def get_all
     @wine_monitors = WineMonitor.all
     @wine_monitors.each do |wine_monitor|
-      @jd.get wine_monitor
+      case wine_monitor.lib
+      when 'Jd'
+        @jd.get wine_monitor
+      when 'Dangdang'
+        @dangdang.get wine_monitor
+      end
     end
   end
 end
