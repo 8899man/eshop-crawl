2fa0348fdf22c0171552f2d0b81bc2a2f0795710
destinyd <destinyd_war@163.com>
2013-11-13 19:15:40 +0800

    wine link

--- app/controllers/user/wine_links_controller.rb
+++ app/controllers/user/wine_links_controller.rb
@@ -1,4 +1,17 @@
 class User::WineLinksController < InheritedResources::Base
+  actions :new, :create
+  def create
+    create! do |success, failure|
+      success.html{
+        if @wine_link.wine_monitor
+          redirect_to @wine_link.wine_monitor
+        else
+          render :create
+        end
+      }
+    end
+  end
+
   protected
   def begin_of_association_chain
     current_user
--- app/controllers/wine_links_controller.rb
+++ app/controllers/wine_links_controller.rb
@@ -1,2 +1,14 @@
 class WineLinksController < InheritedResources::Base
+  actions :new, :create
+  def create
+    create! do |success, failure|
+      success.html{
+        if @wine_link.wine_monitor
+          redirect_to @wine_link.wine_monitor
+        else
+          render :create
+        end
+      }
+    end
+  end
 end
