7a91bc883ba72d4236cab64bb9c142682f79bc81
destinyd <destinyd_war@163.com>
2013-11-13 22:48:09 +0800

    every 2h to get once

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,6 +1,6 @@
 require 'rufus/scheduler'
 
 scheduler = Rufus::Scheduler.new
-scheduler.every "4h", :first_at => Time.now + 60 do
+scheduler.every "2h", :first_at => Time.now + 60 do
   CrawlerController.new.get_all
 end
