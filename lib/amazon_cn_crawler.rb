ba5677068d9f2086b41cd64e6ea0c8554464650c
destinyd <destinyd_war@163.com>
2013-11-15 11:39:17 +0800

    fix regx description too long bug

--- lib/amazon_cn_crawler.rb
+++ lib/amazon_cn_crawler.rb
@@ -4,7 +4,7 @@ class AmazonCnCrawler < Crawler
     @format_url = 'http://www.amazon.cn/dp/%s'
     @regx_price = /(class="listprice">￥ (?<tag_price>[\d\.]+)<\/span>.*)?<b class="priceLarge">￥ (?<current_price>[\d\.]+)<\/b>/m
     @regx_name = /<span id="btAsinTitle">.*?<span style="padding-left: 0" >(?<name>.*?)<\/span>/m
-    @regx_description = [/<div class="h1"><strong>产品信息<\/strong><\/div>.*?<div class="content">(?<description>.*)<\/div>.*?<\/div>.*?<a name="productDetails" id="productDetails">/m, /<h2>商品特性<\/h2>.*?<div class="content">(?<description>.*?)<\/div>/m , /<h2>商品描述<\/h2>.*?<div class="content">(?<description>(?:.*?<div class="emptyClear"> <\/div>){1,}.*?<\/div>).*?(?:importantInformation)?/m ]
+    @regx_description = [/<div class="h1"><strong>产品信息<\/strong><\/div>[\s\n\t]+<div class="content">(?<description>.*)<\/div>[\s\n\t]+<\/div>[\s\n\t]+<a name="productDetails" id="productDetails">/m, /<h2>商品特性<\/h2>[\s\n\t]+<div class="content">(?<description>.*?)<\/div>/m , /<h2>商品描述<\/h2>[\s\n\t]+<div class="content">(?<description>(?:.*?<div class="emptyClear"> <\/div>){1,}.*?<\/div>).*?(?:importantInformation)?/m ]
     @regx_norm = /液体容量<\/span><span>(?<norm>[\d\.]+)( (?<ml>毫)?(?<l>升))?<\/span>/mi
   end
 
