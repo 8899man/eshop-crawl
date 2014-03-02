e0668916ca7260db215ddc2454bc4d8f53d40da2
destinyd <destinyd_war@163.com>
2014-03-02 17:47:41 +0800

    task for long time

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,6 +1,6 @@
 require 'rufus/scheduler'
 
 scheduler = Rufus::Scheduler.new
-scheduler.every "2h", :first_at => Time.now + 60 do
+scheduler.every "6h", :first_at => Time.now + 1800 do
   CrawlerController.new.get_all
 end
